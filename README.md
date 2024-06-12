d425627eed334e93f216f30cea073aa4e9fcab82
Shabab Salehin <auddhayashabab@gmail.com>
2024-06-12 19:42:25 +0600

    admin roll create

---
+++ README.md
@@ -0,0 +1,9 @@
+## Laravel Boilerplate
+Simple boilerplate using Tailwind, Laravel, Laravel Spatie Permissions. 
+
+## Features
+- User Authentication
+- User Roles & Permission
+- Tailwind Admin Panel
+- Laravel Jetstream Support
+- Built with Tailwind & Laravel
---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\Models\User;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = Role::create(['name' => 'admin']);
+
+        $permissions = [
+            ['name' => 'user list'],
+            ['name' => 'create user'],
+            ['name' => 'edit user'],
+            ['name' => 'delete user'],
+            ['name' => 'role list'],
+            ['name' => 'create role'],
+            ['name' => 'edit role'],
+            ['name' => 'delete role'],
+        ];
+
+        foreach ($permissions as $item) {
+            Permission::create($item);
+        }
+
+        $role->syncPermissions(Permission::all());
+
+        $user = User::first(); // Retrieve the first user from the database
+        $user->assignRole($role); // Assign the 'admin' role to the user
+    }
+}
